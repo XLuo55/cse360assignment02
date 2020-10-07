07d6e83f1abd05f9500faf80135e899da1fcf627
Xiang Luo <xluo55@asu.edu>
2020-10-07 05:27:05 -0700

    Initial version

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+/**
+ * CSE 360 
+ * Assignment 1 
+ * @version 1.0 
+ * @author Xiang Luo <xluo55@asu.edu> 
+ */ 
+package cse360assignment02; 
+
+/**
+ * This is the main method. 
+ */ 
+public class AddingMachine {
+	  private int total;
+	  
+	  /**
+	   * This is a constructor. 
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * This method return the current total. 
+	   * @return the current total 
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * This method adds the parameter to the total variable. 
+	   * @param value This is an integer adds to the total variable.
+	   */ 
+	  public void add (int value) {
+	  }
+	  
+	  /**
+	   * This method subtracts the parameter from the total variable. 
+	   * @param value This is an integer subtracts from the total variable.
+	   */ 
+	  public void subtract (int value) {
+	  }
+	  
+	  /**
+	   * This method returns the history of transactions. 
+	   * @return history of transactions 
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /**
+	   * This method clear AddingMachine.
+	   */ 
+	  public void clear() {
+	  }
+	}
